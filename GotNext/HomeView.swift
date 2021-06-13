384692b0428d5b725cd484c3dbbedffb54febd3a
kelvinxlow <l.kelvin687@gmail.com>
2021-06-13 01:36:55 -0400

    Added HomeView

---
+++ GotNext/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GotNext
+//
+//  Created by Kelvin Low on 2021-06-13.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
