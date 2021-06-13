c7c56d1208084bfeb9b5ac20e72a5bc6ba6243fe
kelvinxlow <l.kelvin687@gmail.com>
2021-06-13 00:58:34 -0400

    Initial Commit

---
+++ GotNext/GotNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  GotNextApp.swift
+//  GotNext
+//
+//  Created by Kelvin Low on 2021-06-13.
+//
+
+import SwiftUI
+
+@main
+struct GotNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
