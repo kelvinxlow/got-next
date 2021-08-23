06251dcf390895f9be352f7da1a298608e8b6034
kelvinxlow <l.kelvin687@gmail.com>
2021-08-23 17:44:04 -0400

    008: Add events to Firebase
    - Added logic for adding events to Firebase
    - Fixed the issue with Divider() in LoginView

---
+++ GotNext/Values/FBStrings.swift
@@ -0,0 +1,17 @@
+//
+//  FBStrings.swift
+//  GotNext
+//
+//  Created by Kelvin Low on 2021-08-23.
+//
+
+import Foundation
+
+public class FBStrings {
+    static let events = "events"
+    static let date = "date"
+    static let location = "location"
+    static let description = "description"
+    static let numberOfPeople = "numberOfPeople"
+    static let sender = "sender"
+}
