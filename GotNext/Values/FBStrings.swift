d46fad2c97402e86071c7945f0d429083a32ff45
kelvinxlow <l.kelvin687@gmail.com>
2021-08-24 20:38:41 -0400

    011: Add Event Views
    - Now all the events can be tapped on to see more details
    - Added a new field `name` to events

--- GotNext/Values/FBStrings.swift
+++ GotNext/Values/FBStrings.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public class FBStrings {
     static let events = "events"
+    static let name = "name"
     static let date = "date"
     static let timeSince1970 = "timeSince1970"
     static let location = "location"
--- GotNext/Views/CreateEventView/CreateEventView.swift
+++ GotNext/Views/CreateEventView/CreateEventView.swift
@@ -14,11 +14,12 @@ struct CreateEventView: View {
         case buttonwidth = 128
         case borderWidth = 4
     }
-
+    
     let db = Firestore.firestore()
     
     @Binding public var presentedAsModule: Bool
     @State private var date: Date = Date()
+    @State private var name: String = ""
     @State private var location: String = ""
     @State private var description: String = ""
     @State private var numPeople: Float = 1
@@ -27,97 +28,110 @@ struct CreateEventView: View {
     
     var body: some View {
         VStack(spacing: Spacings.large.rawValue) {
-            // Labels and info fields
-            Group {
-                Text(Strings.createEvent)
-                    .font(.system(size: Fonts.event.rawValue, weight: .medium, design: .serif))
-                    .multilineTextAlignment(.center)
-                
-                Text(Strings.date)
-                    .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
-                    .foregroundColor(Color(Colors.featurePurple))
-                
-                DatePicker(selection: $date, in: Date()..., displayedComponents: [.hourAndMinute, .date]) {
-                }.foregroundColor(Color(Colors.featurePurple))
-                .datePickerStyle(WheelDatePickerStyle())
-                
-                Text(Strings.location)
-                    .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
-                    .foregroundColor(Color(Colors.featurePurple))
-                
-                TextField(Strings.nameAddress,
-                          text: $location)
-                    .autocapitalization(.none)
-                    .padding(Spacings.extraSmall.rawValue)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .background(Color(Colors.backgroundPurple))
-                
-                
-                Text(Strings.description)
-                    .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
-                    .foregroundColor(Color(Colors.featurePurple))
-                
-                TextField(Strings.eventDetails,
-                          text: $description)
-                    .autocapitalization(.none)
-                    .padding(Spacings.extraSmall.rawValue)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .background(Color(Colors.backgroundPurple))
-                
-                Text("\(Strings.people): \(Int(numPeople))")
-                    .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
-                    .foregroundColor(Color(Colors.featurePurple))
-                
-                HStack {
-                    Text("1")
-                        .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
-                        .foregroundColor(Color(Colors.featurePurple))
-                    Slider(value: $numPeople, in: 1...20)
-                        .accentColor(Color(Colors.backgroundPurple))
-                    Text("20")
-                        .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
-                        .foregroundColor(Color(Colors.featurePurple))
-                }
-            }
-            
-            if showErrorMessage {
-                Text(Strings.errorMessage)
-                    .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
-                    .foregroundColor(Color.red)
-                    .multilineTextAlignment(.center)
-            } else if showSuccessMessage {
-                Text(Strings.successMessage)
-                    .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
-                    .foregroundColor(Color.green)
-            }
-            
-            HStack(spacing: Spacings.extraLarge.rawValue) {
-                Button(action: { uploadEventToFirebase() }) {
-                    Text(Strings.confirm)
+            ScrollView {
+                Group {
+                    Text(Strings.createEvent)
+                        .font(.system(size: Fonts.event.rawValue, weight: .medium, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: Spacings.medium.rawValue, trailing: 0))
+                    
+                    Text(Strings.name)
                         .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
-                        .lineLimit(1)
-                        .foregroundColor(Color(Colors.outlineGreen))
-                        .padding(Spacings.large.rawValue)
-                        .frame(width: Sizes.buttonwidth.rawValue, height: Sizes.buttonHeight.rawValue)
-                        .border(Color(Colors.outlineGreen), width: Sizes.borderWidth.rawValue)
+                        .foregroundColor(Color(Colors.featurePurple))
+                    
+                    TextField(Strings.name,
+                              text: $name)
+                        .padding(Spacings.extraSmall.rawValue)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .background(Color(Colors.backgroundPurple))
+                    
+                    Text(Strings.date)
+                        .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
+                        .foregroundColor(Color(Colors.featurePurple))
+                    
+                    DatePicker(selection: $date, in: Date()..., displayedComponents: [.hourAndMinute, .date]) {
+                    }.foregroundColor(Color(Colors.featurePurple))
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .padding(-Spacings.large.rawValue)
                 }
                 
-                Button(action: { presentedAsModule = false }) {
-                    Text(Strings.cancel)
+                Group {
+                    Text(Strings.location)
                         .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
-                        .lineLimit(1)
+                        .foregroundColor(Color(Colors.featurePurple))
+                    
+                    TextField(Strings.address,
+                              text: $location)
+                        .padding(Spacings.extraSmall.rawValue)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .background(Color(Colors.backgroundPurple))
+                    
+                    
+                    Text(Strings.description)
+                        .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
+                        .foregroundColor(Color(Colors.featurePurple))
+                    
+                    TextField(Strings.eventDetails,
+                              text: $description)
+                        .padding(Spacings.extraSmall.rawValue)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .background(Color(Colors.backgroundPurple))
+                    
+                    Text("\(Strings.people): \(Int(numPeople))")
+                        .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
+                        .foregroundColor(Color(Colors.featurePurple))
+                    
+                    HStack {
+                        Text("1")
+                            .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
+                            .foregroundColor(Color(Colors.featurePurple))
+                        Slider(value: $numPeople, in: 1...20)
+                            .accentColor(Color(Colors.backgroundPurple))
+                        Text("20")
+                            .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
+                            .foregroundColor(Color(Colors.featurePurple))
+                    }
+                }
+                
+                if showErrorMessage {
+                    Text(Strings.errorMessage)
+                        .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
                         .foregroundColor(Color.red)
-                        .padding(Spacings.large.rawValue)
-                        .frame(width: Sizes.buttonwidth.rawValue, height: Sizes.buttonHeight.rawValue)
-                        .border(Color.red, width: Sizes.borderWidth.rawValue)
+                        .multilineTextAlignment(.center)
+                } else if showSuccessMessage {
+                    Text(Strings.successMessage)
+                        .font(.system(size: Fonts.header.rawValue, weight: .medium, design: .serif))
+                        .foregroundColor(Color.green)
                 }
-            }.padding(EdgeInsets(top: Spacings.medium.rawValue, leading: 0, bottom: 0, trailing: 0))
-        }.padding(Spacings.huge.rawValue)
-        .preferredColorScheme(.light)
+                
+                HStack(spacing: Spacings.extraLarge.rawValue) {
+                    Button(action: { uploadEventToFirebase() }) {
+                        Text(Strings.confirm)
+                            .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
+                            .lineLimit(1)
+                            .foregroundColor(Color(Colors.outlineGreen))
+                            .padding(Spacings.large.rawValue)
+                            .frame(width: Sizes.buttonwidth.rawValue, height: Sizes.buttonHeight.rawValue)
+                            .border(Color(Colors.outlineGreen), width: Sizes.borderWidth.rawValue)
+                    }
+                    
+                    Button(action: { presentedAsModule = false }) {
+                        Text(Strings.cancel)
+                            .font(.system(size: Fonts.subTitle.rawValue, weight: .medium, design: .serif))
+                            .lineLimit(1)
+                            .foregroundColor(Color.red)
+                            .padding(Spacings.large.rawValue)
+                            .frame(width: Sizes.buttonwidth.rawValue, height: Sizes.buttonHeight.rawValue)
+                            .border(Color.red, width: Sizes.borderWidth.rawValue)
+                    }
+                }.padding(EdgeInsets(top: Spacings.medium.rawValue, leading: 0, bottom: 0, trailing: 0))
+            }.padding(Spacings.huge.rawValue)
+            .preferredColorScheme(.light)
+        }
     }
     
     private func uploadEventToFirebase() {
-        if location != "", description != "", let sender = Auth.auth().currentUser?.email {
+        if name != "", location != "", description != "", let sender = Auth.auth().currentUser?.email {
             let dateFormatter = DateFormatter()
             dateFormatter.dateStyle = .full
             dateFormatter.timeStyle = .short
@@ -126,20 +140,21 @@ struct CreateEventView: View {
             let timeSince1970 = date.timeIntervalSince1970
             
             db.collection(FBStrings.events).addDocument(data:
-                                                            [FBStrings.date : stringDate,
+                                                            [FBStrings.name: name,
+                                                             FBStrings.date: stringDate,
                                                              FBStrings.timeSince1970: timeSince1970,
                                                              FBStrings.location: location,
                                                              FBStrings.description: description,
                                                              FBStrings.numberOfPeople: Int(numPeople),
                                                              FBStrings.sender: sender
-            ]) { (error) in
+                                                            ]) { (error) in
                 if error != nil {
                     showError()
                 }
                 else {
                     showErrorMessage = false
                     showSuccessMessage = true
-                   
+                    
                     resetValues()
                 }
             }
@@ -158,6 +173,7 @@ struct CreateEventView: View {
         date = Date()
         location = ""
         description = ""
+        name = ""
         numPeople = 1
     }
 }
